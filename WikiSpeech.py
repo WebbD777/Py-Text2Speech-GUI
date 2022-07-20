ca08c768e25180814f063071551f022f4f5779bf
WebbD777 <109418123+WebbD777@users.noreply.github.com>
2022-07-20 22:39:20 +0200

    The project works, text2speech enabled

--- WikiSpeech.py
+++ WikiSpeech.py
@@ -4,7 +4,9 @@ import pyttsx3
 import wikipedia
 import tkinter as tk
 
-from tkinter import filedialog, Text, BOTH
+from tkinter import filedialog, Text, BOTH, END
+
+search = ""
 
 # import tweepy
 #Create an instance of Tkinter frame
@@ -15,7 +17,11 @@ win.geometry("750x250")
 
 def getSearch():
     global entry
-    string = entry.get()
+
+    engine = pyttsx3.init()
+    engine.say(wikipedia.summary(entry.get(), sentences=3))
+    engine.runAndWait()
+
 
 
 #root = tk.Tk()
@@ -29,8 +35,10 @@ label.pack(pady=14)
 entry = tk.Entry(canvas, width=40)
 entry.pack(pady=14)
 
-inputButton = tk.Button(canvas, text="Search", command=getSearch())#, padx=10, pady=5, fg='black', bd='#263D42')
+inputButton = tk.Button(canvas, text="Search", command=getSearch)#, padx=10, pady=5, fg='black', bd='#263D42')
 inputButton.pack(pady=14)
+
+print(search)
 
 win.mainloop()
 #root.mainloop()
@@ -40,6 +48,10 @@ win.mainloop()
 engine = pyttsx3.init()
 engine.say(wikipedia.summary("Rohit Sharma", sentences=3))
 engine.runAndWait()
+
+engine = pyttsx3.init()
+    engine.say(op)
+    engine.runAndWait()
 
 # print(wikipedia.summary("Rohit Sharma", sentences=3))
 """
