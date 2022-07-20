b08c094128e7e93e1f2f12817612409c56ef746f
WebbD777 <109418123+WebbD777@users.noreply.github.com>
2022-07-20 21:09:53 +0200

    Started project

---
+++ WikiSpeech.py
@@ -0,0 +1,37 @@
+# import os
+# import sys
+import pyttsx3
+import wikipedia
+import tkinter as tk
+
+from tkinter import filedialog, Text, BOTH
+
+# import tweepy
+#Create an instance of Tkinter frame
+win = tk.Tk()
+#Set the geometry of Tkinter frame
+win.geometry("750x250")
+
+#root = tk.Tk()
+
+canvas = tk.Canvas(height=650, width=700, bg="bisque")
+canvas.pack(fill= BOTH, expand= True)
+
+label = tk.Label(canvas , text="Search Wikipedia")
+label.pack(pady=14)
+
+entry = tk.Entry()
+entry.pack(pady=14)
+
+
+win.mainloop()
+#root.mainloop()
+
+
+"""
+engine = pyttsx3.init()
+engine.say(wikipedia.summary("Rohit Sharma", sentences=3))
+engine.runAndWait()
+
+# print(wikipedia.summary("Rohit Sharma", sentences=3))
+"""
