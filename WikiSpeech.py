7cc21d6896f5234072c0de743a83099fb6013070
WebbD777 <109418123+WebbD777@users.noreply.github.com>
2022-07-20 22:47:58 +0200

    cleaned up code

--- WikiSpeech.py
+++ WikiSpeech.py
@@ -1,14 +1,12 @@
-# import os
-# import sys
+
 import pyttsx3
 import wikipedia
 import tkinter as tk
 
-from tkinter import filedialog, Text, BOTH, END
+from tkinter import BOTH
 
 search = ""
 
-# import tweepy
 #Create an instance of Tkinter frame
 win = tk.Tk()
 #Set the geometry of Tkinter frame
@@ -29,10 +27,10 @@ def getSearch():
 canvas = tk.Canvas(height=650, width=700, bg="bisque")
 canvas.pack(fill= BOTH, expand= True)
 
-label = tk.Label(canvas , text="Search Wikipedia", font=(None, 23))
+label = tk.Label(canvas , text="Input phrase for to listen to Wiki entry", font=(None, 23))
 label.pack(pady=14)
 
-entry = tk.Entry(canvas, width=40)
+entry = tk.Entry(canvas ,width=40)
 entry.pack(pady=14)
 
 inputButton = tk.Button(canvas, text="Search", command=getSearch)#, padx=10, pady=5, fg='black', bd='#263D42')
@@ -41,17 +39,3 @@ inputButton.pack(pady=14)
 print(search)
 
 win.mainloop()
-#root.mainloop()
-
-
-"""
-engine = pyttsx3.init()
-engine.say(wikipedia.summary("Rohit Sharma", sentences=3))
-engine.runAndWait()
-
-engine = pyttsx3.init()
-    engine.say(op)
-    engine.runAndWait()
-
-# print(wikipedia.summary("Rohit Sharma", sentences=3))
-"""
